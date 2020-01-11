6b7e17602c910341e4aab364f52a8bcf5068e1e1
ncaldwell17 <noahcaldwell14@yahoo.com>
2020-01-10 18:22:30 -0600

    added login capabilities

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 # remember that routes.py functions are called VIEW functions
 
 # this callback creates an association b/w this function and the url
 @app.route('/')
 @app.route('/index')
+@app.route('/about')
 def index():
-    return "Hello World!"
+    form = LoginForm()
+    # user = {'username': 'Rolo'}
+    return render_template('index.html', 
+                           title='Sign In to Rolo!',
+                           form=form)
