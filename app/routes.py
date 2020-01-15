f40388c2d7dfc0b838f3efd022c4a3d208a541a1
ncaldwell17 <noahcaldwell14@yahoo.com>
2020-01-14 19:13:49 -0600

    bootstrap flask integration

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,11 @@ from app.forms import LoginForm
 
 # remember that routes.py functions are called VIEW functions
 
+
 # this callback creates an association b/w this function and the url
 @app.route('/')
 @app.route('/index')
-@app.route('/about')
 def index():
     form = LoginForm()
-    # user = {'username': 'Rolo'}
-    return render_template('index.html', 
-                           title='Sign In to Rolo!',
-                           form=form)
+    # user = {'username': 'Rolodev'}
+    return render_template('main.html', form=form)
