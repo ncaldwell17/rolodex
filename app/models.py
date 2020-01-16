39e884c9b1f73f0ea5e6b29923e10aee6b93859b
ncaldwell17 <noahcaldwell14@yahoo.com>
2020-01-16 00:03:50 -0600

    login information

--- app/models.py
+++ app/models.py
@@ -25,11 +25,18 @@ access anything after you've created a class from another class, use the snake-c
 instead of the class name.
 """
 
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -39,6 +46,12 @@ class User(db.Model):
     # tells Python how to print objects of this class, which is useful for debugging
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Rolodex(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,38 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from flask_login import current_user, login_user, logout_user
 
 # remember that routes.py functions are called VIEW functions
 
 
 # this callback creates an association b/w this function and the url
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    # prevents a logged in user from accessing the login page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    # handles the login information
     form = LoginForm()
-    # user = {'username': 'Rolodev'}
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
     return render_template('main.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
